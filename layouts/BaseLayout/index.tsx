a1bea2826d8b8364f075b82f25b6a1a1ad314e07
kaido <maxartem0419@gmail.com>
2021-11-10 17:15:12 +0300

    Created the bottom information on how the order is formed

--- layouts/BaseLayout/index.tsx
+++ layouts/BaseLayout/index.tsx
@@ -1,6 +1,7 @@
 // Components
 import { HeaderComponent } from "../../components/Header"
 import { FooterComponent } from "../../components/Footer"
+import { HowThisWorkComponent } from '../../components/HowThisWork'
 interface ILayoutProps {
   children: React.ReactNode
 }
@@ -9,8 +10,11 @@ export const BaseLayout: React.FC = ({ children }) => {
   return (
     <>
       <HeaderComponent />
-      <main className="main-container">
-        {children}
+      <main>
+        <div className="main-container">
+          {children}
+          <HowThisWorkComponent />
+        </div>
       </main>
       <FooterComponent />
     </>
